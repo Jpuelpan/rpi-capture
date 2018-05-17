d5ba76283db596df6085c9dd27597304ac99c32a
Juan Puelpan <jpuelpan528@gmail.com>
2018-05-17 17:28:35 -0400

    Some changes

--- capture.py
+++ capture.py
@@ -6,24 +6,28 @@ import request
 from time import sleep
 from picamera import PiCamera
 
-PICTURES_FOLDER = os.path.join(os.getcwd(), 'pictures')
-UPLOAD_URL = 'http://localhost:5000/api/v1/media'
+WAIT_TIME = 300 # 5 minutes
+PICTURES_FOLDER = os.path.join('/', 'tmp')
+UPLOAD_URL = 'http://localhost:5000/api/v1/media?deviceId='
 
-def take_picture():
-    camera = PiCamera()
-    camera.resolution = (1024, 768)
-    camera.start_preview()
-    # Camera warm-up time
-    sleep(2)
-    filename = os.path.join(PICTURES_FOLDER, str(uuid.uuid1()) + '.jpg')
-    camera.capture(filename)
-    return filename
+while true:
+    def take_picture():
+        camera = PiCamera()
+        camera.resolution = (1024, 768)
+        camera.start_preview()
+        # Camera warm-up time
+        sleep(2)
+        filename = os.path.join(PICTURES_FOLDER, str(uuid.uuid1()) + '.jpg')
+        camera.capture(filename)
+        return filename
 
-def upload_picture(filepath):
-    files = {'file': open(filepath, 'rb')}
-    res = requests.post(UPLOAD_URL, files=files)
+    def upload_picture(filepath):
+        files = { 'file': open(filepath, 'rb') }
+        res = requests.post(UPLOAD_URL, files=files)
+        return res
 
-    return res
+    picture_file = take_picture()
+    upload_picture(picture_file)
 
-picture_file = take_picture()
-upload_picture(picture_file)
+    time.sleep(WAIT_TIME)
+
